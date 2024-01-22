138199bd5898c9f7864017865af5a1fea43c3910
CyresSmith <107186155+CyresSmith@users.noreply.github.com>
2024-01-23 01:00:59 +0200

    add new service

--- src/services/dto/create-service.dto.ts
+++ src/services/dto/create-service.dto.ts
@@ -1,1 +1,52 @@
-export class CreateServiceDto {}
+import {
+  IsArray,
+  IsDefined,
+  IsNumber,
+  IsOptional,
+  IsString,
+  Length,
+} from 'class-validator';
+import { Category, Resource } from 'db/entities';
+
+export class CreateServiceDto {
+  @IsString()
+  @Length(3, 20)
+  name: string;
+
+  @IsString()
+  @IsOptional()
+  avatar: string;
+
+  @IsNumber()
+  @IsDefined()
+  duration: number;
+
+  @IsNumber()
+  @IsOptional()
+  break: number;
+
+  @IsNumber()
+  @IsDefined()
+  price: number;
+
+  @IsString()
+  @Length(10, 500)
+  @IsOptional()
+  desc: string;
+
+  @IsArray()
+  @IsOptional()
+  images: string[];
+
+  @IsNumber()
+  @IsDefined()
+  category: number;
+
+  @IsArray()
+  @IsOptional()
+  employees: number[];
+
+  @IsArray()
+  @IsOptional()
+  resources: number[];
+}
