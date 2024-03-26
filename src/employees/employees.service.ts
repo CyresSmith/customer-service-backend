f835dba0d053b8769a34cde9f3582a5fcb1ad9a7
CyresSmith <107186155+CyresSmith@users.noreply.github.com>
2024-03-26 12:02:04 +0200

    fixes

--- src/employees/employees.service.ts
+++ src/employees/employees.service.ts
@@ -177,7 +177,7 @@ export class EmployeesService {
 
   // ======================================== Add employee service
 
-  async addEmployeeService(
+  async updateEmployeeServices(
     companyId: number,
     services: number[],
     employeeId: number
@@ -192,12 +192,9 @@ export class EmployeesService {
 
     if (!isExist) throw new BadRequestException('Співробітника не знайдено');
 
-    isExist.services = [
-      ...isExist.services,
-      ...(services.map(id => ({
-        id,
-      })) as Service[]),
-    ];
+    isExist.services = services.map(id => ({
+      id,
+    })) as Service[];
 
     return await this.employeesRepository.save(isExist);
   }
